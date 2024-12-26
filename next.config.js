a0bb83b6f20fee29e3095e3a8deca8d6b903f5b1
Daniel Lang <danslang@gmail.com>
2024-12-26 11:38:42 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone'
+  distDir: '.next',
+  experimental: {
+    appDir: true
+  }
 }
 
 module.exports = nextConfig
