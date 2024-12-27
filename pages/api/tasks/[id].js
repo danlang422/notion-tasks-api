ff257296e344a28076a410e01e867704ac0ce845
Daniel Lang <danslang@gmail.com>
2024-12-27 10:11:09 -0600

    Update [id].js
    Additional fields from Notion DB

--- pages/api/tasks/[id].js
+++ pages/api/tasks/[id].js
@@ -1,5 +1,5 @@
-// api/tasks/[id].js
-const { Client } = require('@notionhq/client');
+// pages/api/tasks/[id].js
+import { Client } from '@notionhq/client';
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -9,18 +9,30 @@ export default async function handler(req, res) {
   const { id } = req.query;
 
   if (req.method === 'PATCH') {
-    const { status } = req.body;
+    const { status, timeframe, areaIds, frameId, doDate } = req.body;
     
     try {
+      const properties = {};
+      
+      if (status) {
+        properties.Status = { status: { name: status } };
+      }
+      if (timeframe) {
+        properties.Timeframe = { select: { name: timeframe } };
+      }
+      if (areaIds) {
+        properties.Areas = { relation: areaIds.map(id => ({ id })) };
+      }
+      if (frameId) {
+        properties.Frame = { relation: [{ id: frameId }] };
+      }
+      if (doDate) {
+        properties["Do Date"] = { date: { start: doDate } };
+      }
+
       const response = await notion.pages.update({
         page_id: id,
-        properties: {
-          Status: {
-            select: {
-              name: status,
-            },
-          },
-        },
+        properties
       });
       
       res.status(200).json(response);
